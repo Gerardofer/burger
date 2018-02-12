7d9eb705fae0199fa7f348b0f3fbfffb4392df0a
Gerardo Fernandez <gerardojose13@gmail.com>
2018-02-11 20:07:34 -0800

    adds new files

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -13,8 +13,25 @@ app.get("/", (req, res) => {
     });
 });
 
-app.post("/api/burger", (req, res) => {
+app.post("/api/burgers", (req, res) => {
     burger.create([burger_name, devoured], [req.body.burger_name, req.body.devoured], (res) => {
         res.json({id: result.insertId});
-    })
-})
+    });
+});
+
+app.put("/api/burgers/:id", (req, res) => {
+    let condition = "id = " + req.params.id;
+    console.log("condition", condition);
+
+    car.update({devoured: req.body.devoured}, condition, (res) => {
+        if (res.changedRows == 0){
+            return res.status(404).end();
+        }
+        else {
+            res.status(200).end();
+        }
+    });
+});
+
+
+module.exports = app;
